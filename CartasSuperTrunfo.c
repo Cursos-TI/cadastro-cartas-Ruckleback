971a8dee301a89a5ee863148571478c20f06aa23
Felipe Rodrigues Celestino <feliperce@hotmail.com>
2026-06-07 19:29:02 +0000

    Finalizado o desafio Super Trunfo módulo Mestre.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -22,7 +22,8 @@ int main() {
       int A01_PontosTuristicos, A02_PontosTuristicos, A03_PontosTuristicos, A04_PontosTuristicos;//Pontos turísticos das cidades
       float A01_PIB_perCapita, A02_PIB_perCapita, A03_PIB_perCapita, A04_PIB_perCapita;//PIB per capita das cidades
       float A01_DensidadePopulacional, A02_DensidadePopulacional, A03_DensidadePopulacional, A04_DensidadePopulacional;//Densidade populacional das cidades
-
+      float A01_SuperTrunfo, A02_SuperTrunfo, A03_SuperTrunfo, A04_SuperTrunfo;//Valor somado de todas as variáveis da carta para definir o Super Trunfo
+    
    
     char deck2[20];//Deck 2 e cartas
         char B01, B02, B03, B04; //ID das cidades
@@ -33,7 +34,7 @@ int main() {
         int B01_PontosTuristicos, B02_PontosTuristicos, B03_PontosTuristicos, B04_PontosTuristicos;//Pontos turísticos das cidades
         float B01_PIB_perCapita, B02_PIB_perCapita, B03_PIB_perCapita, B04_PIB_perCapita;//PIB per capita das cidades
         float B01_DensidadePopulacional, B02_DensidadePopulacional, B03_DensidadePopulacional, B04_DensidadePopulacional;//Densidade populacional das cidades
-
+        float B01_SuperTrunfo, B02_SuperTrunfo, B03_SuperTrunfo, B04_SuperTrunfo;//Valor somado de todas as variáveis da carta para definir o Super Trunfo
       
 
     
@@ -63,6 +64,7 @@ int main() {
     scanf ("%f", &A01_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &A01_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados
 
     printf ("Carta 2:");
     scanf ("%s", A02_nome);
@@ -74,6 +76,7 @@ int main() {
     scanf ("%f", &A02_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &A02_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados
 
     printf ("Carta 3:");
     scanf ("%s", A03_nome);
@@ -85,6 +88,7 @@ int main() {
     scanf ("%f", &A03_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &A03_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados
 
     printf ("Carta 4:");
     scanf ("%s", A04_nome);
@@ -96,6 +100,8 @@ int main() {
     scanf ("%f", &A04_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &A04_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados 
+
     //Cálculos
 A01_DensidadePopulacional = A01_populacao/A01_area;
 A01_PIB_perCapita = A01_PIB/A01_populacao;
@@ -105,6 +111,10 @@ A03_DensidadePopulacional = A03_populacao/A03_area;
 A03_PIB_perCapita = A03_PIB/A03_populacao;
 A04_DensidadePopulacional = A04_populacao/A04_area;
 A04_PIB_perCapita = A04_PIB/A04_populacao;
+A01_SuperTrunfo = A01_populacao + A01_PIB + A01_area + A01_PontosTuristicos + (1.00 / A01_DensidadePopulacional) + A01_PIB_perCapita;
+A02_SuperTrunfo = A02_populacao + A02_PIB + A02_area + A02_PontosTuristicos + (1.00 / A02_DensidadePopulacional) + A02_PIB_perCapita;
+A03_SuperTrunfo = A03_populacao + A03_PIB + A03_area + A03_PontosTuristicos + (1.00 / A03_DensidadePopulacional) + A03_PIB_perCapita;
+A04_SuperTrunfo = A04_populacao + A04_PIB + A04_area + A04_PontosTuristicos + (1.00 / A04_DensidadePopulacional) + A04_PIB_perCapita;
 
 //Confirmação dos dados do Player 1
 printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
@@ -115,33 +125,43 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     printf ("Área: %.2f\n", A01_area);
     printf ("Pontos turísticos: %d\n", A01_PontosTuristicos);
 
-
     printf ("Densidade Populacional: %.2f\n", A01_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", A01_PIB_perCapita);
+    printf ("Super Trunfo: %.2f\n", A01_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
 
     printf ("Carta 2: %s\n", A02_nome);
     printf ("População: %.2f\n", A02_populacao);
     printf ("PIB: %.2f\n", A02_PIB);
     printf ("Área: %.2f\n", A02_area);
     printf ("Pontos turísticos: %d\n", A02_PontosTuristicos);
+    
     printf ("Densidade Populacional: %.2f\n", A02_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", A02_PIB_perCapita);
+    printf ("Super Trunfo: %.2f\n", A02_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
 
     printf ("Carta 3: %s\n", A03_nome);
     printf ("População: %.2f\n", A03_populacao);
     printf ("PIB: %.2f\n", A03_PIB);
     printf ("Área: %.2f\n", A03_area);
     printf ("Pontos turísticos: %d\n", A03_PontosTuristicos);
+
     printf ("Densidade Populacional: %.2f\n", A03_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", A03_PIB_perCapita);
+    printf ("Super Trunfo: %.2f\n", A03_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
 
     printf ("Carta 4: %s\n", A04_nome);
     printf ("População: %.2f\n", A04_populacao);
     printf ("PIB: %.2f\n", A04_PIB);
     printf ("Área: %.2f\n", A04_area);
     printf ("Pontos turísticos: %d\n", A04_PontosTuristicos);
+
     printf ("Densidade Populacional: %.2f\n", A04_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", A04_PIB_perCapita);
+    printf ("Super Trunfo: %.2f\n", A04_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
 
     printf ("Dados cadastrados com sucesso! Agora é a vez do Player 2 cadastrar suas cartas.\n");
 
@@ -167,6 +187,7 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     scanf ("%f", &B01_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &B01_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados
    
     printf ("Carta 2:");
     scanf ("%s", B02_nome);
@@ -178,6 +199,7 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     scanf ("%f", &B02_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &B02_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados
 
     printf ("Carta 3:");
     scanf ("%s", B03_nome);
@@ -189,7 +211,8 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     scanf ("%f", &B03_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &B03_PontosTuristicos);
-    
+    printf ("\n");//quebra de linha para melhor visualização dos dados
+
     printf ("Carta 4:");
     scanf ("%s", B04_nome);   
     printf ("População da cidade:\n");
@@ -200,6 +223,7 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     scanf ("%f", &B04_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &B04_PontosTuristicos);
+    printf ("\n");//quebra de linha para melhor visualização dos dados
 
     //Cálculos
     B01_DensidadePopulacional = B01_populacao/B01_area;
@@ -211,6 +235,11 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     B04_DensidadePopulacional = B04_populacao/B04_area;
     B04_PIB_perCapita = B04_PIB/B04_populacao;
 
+    B01_SuperTrunfo = B01_populacao + B01_PIB + B01_area + B01_PontosTuristicos + (1.00 / B01_DensidadePopulacional) + B01_PIB_perCapita;
+    B02_SuperTrunfo = B02_populacao + B02_PIB + B02_area + B02_PontosTuristicos + (1.00 / B02_DensidadePopulacional) + B02_PIB_perCapita;
+    B03_SuperTrunfo = B03_populacao + B03_PIB + B03_area + B03_PontosTuristicos + (1.00 / B03_DensidadePopulacional) + B03_PIB_perCapita;
+    B04_SuperTrunfo = B04_populacao + B04_PIB + B04_area + B04_PontosTuristicos + (1.00 / B04_DensidadePopulacional) + B04_PIB_perCapita;
+
 printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
 
     printf ("***Confirmação dos dados:***\n");
@@ -219,39 +248,71 @@ printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
     printf ("PIB: %.2f\n", B01_PIB);
     printf ("Área: %.2f\n", B01_area);
     printf ("Pontos turísticos: %d\n", B01_PontosTuristicos);
+
     printf ("Densidade Populacional: %.2f\n", B01_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", B01_PIB_perCapita);
+    printf ("Super Trunfo: %.2f\n", B01_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
 
     printf ("Carta 2: %s\n", B02_nome);
     printf ("População: %.2f\n", B02_populacao);
     printf ("PIB: %.2f\n", B02_PIB);
     printf ("Área: %.2f\n", B02_area);
     printf ("Pontos turísticos: %d\n", B02_PontosTuristicos);
+
     printf ("Densidade Populacional: %.2f\n", B02_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", B02_PIB_perCapita);
+    printf ("Super Trunfo: %.2f\n", B02_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
     
     printf ("Carta 3: %s\n", B03_nome);
     printf ("População: %.2f\n", B03_populacao);
     printf ("PIB: %.2f\n", B03_PIB);
     printf ("Área: %.2f\n", B03_area);
     printf ("Pontos turísticos: %d\n", B03_PontosTuristicos);
+    
     printf ("Densidade Populacional: %.2f\n", B03_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", B03_PIB_perCapita);
-    
+    printf ("Super Trunfo: %.2f\n", B03_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
+
     printf ("Carta 4: %s\n", B04_nome);
     printf ("População: %.2f\n", B04_populacao);
     printf ("PIB: %.2f\n", B04_PIB);
     printf ("Área: %.2f\n", B04_area);
     printf ("Pontos turísticos: %d\n", B04_PontosTuristicos);
+
     printf ("Densidade Populacional: %.2f\n", B04_DensidadePopulacional);
     printf ("PIB per Capita: %.2f\n", B04_PIB_perCapita);
-    
+    printf ("Super Trunfo: %.2f\n", B04_SuperTrunfo);
+ printf ("\n");//quebra de linha para melhor visualização dos dados 
+
 printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
 
     printf ("Dados cadastrados com sucesso! Agora é hora de jogar!\n");
     
 
-  // Área para exibição dos dados da cidade
+  // Área para exibição do combate
+
+  printf ("Comparando as cartas...\n");
+    printf ("Carta 1 do Player 1: %s\n", A01_nome);
+    printf ("Carta 1 do Player 2: %s\n", B01_nome);
+
+printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
+    
+    printf ("!!!SuperTrunfos ao combate!!!\n");
+    printf ("Super Trunfo da Carta 1 do Player 1: %.2f\n", A01_SuperTrunfo);
+    printf ("Super Trunfo da Carta 1 do Player 2: %.2f\n", B01_SuperTrunfo);
+
+printf("\n*** *** ***\n");//quebra de linha para melhor visualização dos dados
+
+    if (A01_SuperTrunfo > B01_SuperTrunfo) {
+        printf ("Player 1 venceu a rodada com a carta %s!\n", A01_nome);
+    } else if (B01_SuperTrunfo > A01_SuperTrunfo) {
+        printf ("Player 2 venceu a rodada com a carta %s!\n", B01_nome);
+    } else {
+        printf ("Empate! Ambas as cartas têm o mesmo valor de Super Trunfo.\n");
+    }
 
     
 
