9e28d6b2431a7da291aeeff25a21ccf41fa8390d
Felipe Rodrigues Celestino <feliperce@hotmail.com>
2026-06-07 06:05:07 +0000

    //modified:   CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,15 +7,15 @@
 
 int main() {
     setlocale(LC_ALL, "Portuguese_brazil");
-  //Players e Decks  
+  //Player 1 e Player 2
   char Player1[20];
   char Player2[20];
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
   
-    char deck1;//Deck 1 e cartas
+      char deck1[20];//Deck 1 e cartas
       char A01, A02, A03, A04; //ID das cidades
-      char A01_nome, A02_nome, A03_nome, A04_nome;//Nomes das cidades
+      char A01_nome[50], A02_nome[50], A03_nome[50], A04_nome[50];//Nomes das cidades
       float A01_populacao, A02_populacao, A03_populacao, A04_populacao;//Populações das cidades
       float A01_PIB, A02_PIB, A03_PIB, A04_PIB;//PIB das cidades
       float A01_area, A02_area, A03_area, A04_area;//Áreas das cidades
@@ -24,9 +24,9 @@ int main() {
       float A01_DensidadePopulacional, A02_DensidadePopulacional, A03_DensidadePopulacional, A04_DensidadePopulacional;//Densidade populacional das cidades
 
    
-    char deck2;//Deck 2 e cartas
+    char deck2[20];//Deck 2 e cartas
         char B01, B02, B03, B04; //ID das cidades
-        char B01_nome, B02_nome, B03_nome, B04_nome;//Nomes das cidades
+        char B01_nome[50], B02_nome[50], B03_nome[50], B04_nome[50];//Nomes das cidades
         float B01_populacao, B02_populacao, B03_populacao, B04_populacao;//Populações das cidades
         float B01_PIB, B02_PIB, B03_PIB, B04_PIB;//PIB das cidades
         float B01_area, B02_area, B03_area, B04_area;//Áreas das cidades
@@ -54,7 +54,7 @@ int main() {
 //Cadastro das cartas do Player 1
     printf ("Cadastrar cartas do Deck 1...\n");
     printf ("Carta 1:");
-    scanf ("%s", &A01_nome);
+    scanf ("%s", A01_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &A01_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -65,7 +65,7 @@ int main() {
     scanf ("%d", &A01_PontosTuristicos);
 
     printf ("Carta 2:");
-    scanf ("%s", &A02_nome);
+    scanf ("%s", A02_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &A02_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -76,7 +76,7 @@ int main() {
     scanf ("%d", &A02_PontosTuristicos);
 
     printf ("Carta 3:");
-    scanf ("%s", &A03_nome);
+    scanf ("%s", A03_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &A03_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -87,7 +87,7 @@ int main() {
     scanf ("%d", &A03_PontosTuristicos);
 
     printf ("Carta 4:");
-    scanf ("%s", &A04_nome);
+    scanf ("%s", A04_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &A04_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -96,41 +96,51 @@ int main() {
     scanf ("%f", &A04_area);
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &A04_PontosTuristicos);
+    //Cálculos
+A01_DensidadePopulacional = A01_populacao/A01_area;
+A01_PIB_perCapita = A01_PIB/A01_populacao;
+A02_DensidadePopulacional = A02_populacao/A02_area;
+A02_PIB_perCapita = A02_PIB/A02_populacao;
+A03_DensidadePopulacional = A03_populacao/A03_area;
+A03_PIB_perCapita = A03_PIB/A03_populacao;
+A04_DensidadePopulacional = A04_populacao/A04_area;
+A04_PIB_perCapita = A04_PIB/A04_populacao;
 
-    
 //Confirmação dos dados do Player 1
-    printf ("Confirmação dos dados:\n");
-    printf ("Carta 1: %s\n", &A01_nome);
-    printf ("População: %.2f\n", &A01_populacao);
-    printf ("PIB: %.2f\n", &A01_PIB);
-    printf ("Área: %.2f\n", &A01_area);
-    printf ("Pontos turísticos: %d\n", &A01_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &A01_DensidadePopulacional, A01_DensidadePopulacional = A01_populacao/A01_area);
-    printf ("PIB per Capita: %.2f\n", &A01_PIB_perCapita, A01_PIB_perCapita = A01_PIB/A01_populacao);
+    printf ("***Confirmação dos dados:***\n");
+    printf ("Carta 1: %s\n", A01_nome);
+    printf ("População: %.2f\n", A01_populacao);
+    printf ("PIB: %.2f\n", A01_PIB);
+    printf ("Área: %.2f\n", A01_area);
+    printf ("Pontos turísticos: %d\n", A01_PontosTuristicos);
 
-    printf ("Carta 2: %s\n", &A02_nome);
-    printf ("População: %.2f\n", &A02_populacao);
-    printf ("PIB: %.2f\n", &A02_PIB);
-    printf ("Área: %.2f\n", &A02_area);
-    printf ("Pontos turísticos: %d\n", &A02_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &A02_DensidadePopulacional, A02_DensidadePopulacional = A02_populacao/A02_area);
-    printf ("PIB per Capita: %.2f\n", &A02_PIB_perCapita, A02_PIB_perCapita = A02_PIB/A02_populacao);
 
-    printf ("Carta 3: %s\n", &A03_nome);
-    printf ("População: %.2f\n", &A03_populacao);
-    printf ("PIB: %.2f\n", &A03_PIB);
-    printf ("Área: %.2f\n", &A03_area);
-    printf ("Pontos turísticos: %d\n", &A03_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &A03_DensidadePopulacional, A03_DensidadePopulacional = A03_populacao/A03_area);
-    printf ("PIB per Capita: %.2f\n", &A03_PIB_perCapita, A03_PIB_perCapita = A03_PIB/A03_populacao);
+    printf ("Densidade Populacional: %.2f\n", A01_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", A01_PIB_perCapita);
 
-    printf ("Carta 4: %s\n", &A04_nome);
-    printf ("População: %.2f\n", &A04_populacao);
-    printf ("PIB: %.2f\n", &A04_PIB);
-    printf ("Área: %.2f\n", &A04_area);
-    printf ("Pontos turísticos: %d\n", &A04_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &A04_DensidadePopulacional, A04_DensidadePopulacional = A04_populacao/A04_area);
-    printf ("PIB per Capita: %.2f\n", &A04_PIB_perCapita, A04_PIB_perCapita = A04_PIB/A04_populacao);
+    printf ("Carta 2: %s\n", A02_nome);
+    printf ("População: %.2f\n", A02_populacao);
+    printf ("PIB: %.2f\n", A02_PIB);
+    printf ("Área: %.2f\n", A02_area);
+    printf ("Pontos turísticos: %d\n", A02_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", A02_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", A02_PIB_perCapita);
+
+    printf ("Carta 3: %s\n", A03_nome);
+    printf ("População: %.2f\n", A03_populacao);
+    printf ("PIB: %.2f\n", A03_PIB);
+    printf ("Área: %.2f\n", A03_area);
+    printf ("Pontos turísticos: %d\n", A03_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", A03_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", A03_PIB_perCapita);
+
+    printf ("Carta 4: %s\n", A04_nome);
+    printf ("População: %.2f\n", A04_populacao);
+    printf ("PIB: %.2f\n", A04_PIB);
+    printf ("Área: %.2f\n", A04_area);
+    printf ("Pontos turísticos: %d\n", A04_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", A04_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", A04_PIB_perCapita);
 
     printf ("Dados cadastrados com sucesso! Agora é a vez do Player 2 cadastrar suas cartas.\n");
 //fim do cadastro do Player 1, início do cadastro do Player 2
@@ -141,11 +151,11 @@ int main() {
     scanf ("%s", Player2);
 
     printf ("Insira o nome do deck do Player 2:\n");
-    scanf ("%s", &deck2);
+    scanf ("%s", deck2);
 
     printf ("Cadastrar cartas do Deck 2...\n");
     printf ("Carta 1:");
-    scanf ("%s", &B01_nome);
+    scanf ("%s", B01_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &B01_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -156,7 +166,7 @@ int main() {
     scanf ("%d", &B01_PontosTuristicos);
    
     printf ("Carta 2:");
-    scanf ("%s", &B02_nome);
+    scanf ("%s", B02_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &B02_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -167,7 +177,7 @@ int main() {
     scanf ("%d", &B02_PontosTuristicos);
 
     printf ("Carta 3:");
-    scanf ("%s", &B03_nome);
+    scanf ("%s", B03_nome);
     printf ("População da cidade:\n");
     scanf ("%f", &B03_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -178,7 +188,7 @@ int main() {
     scanf ("%d", &B03_PontosTuristicos);
     
     printf ("Carta 4:");
-    scanf ("%s", &B04_nome);   
+    scanf ("%s", B04_nome);   
     printf ("População da cidade:\n");
     scanf ("%f", &B04_populacao);
     printf ("PIB da cidade (R$ em Bilhões):\n");
@@ -188,39 +198,48 @@ int main() {
     printf ("Número de pontos turísticos da cidade:\n");
     scanf ("%d", &B04_PontosTuristicos);
 
-    
-    printf ("Confirmação dos dados:\n");
-    printf ("Carta 1: %s\n", &B01_nome);
-    printf ("População: %.2f\n", &B01_populacao);
-    printf ("PIB: %.2f\n", &B01_PIB);
-    printf ("Área: %.2f\n", &B01_area);
-    printf ("Pontos turísticos: %d\n", &B01_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &B01_DensidadePopulacional, B01_DensidadePopulacional = B01_populacao/B01_area);
-    printf ("PIB per Capita: %.2f\n", &B01_PIB_perCapita, B01_PIB_perCapita = B01_PIB/B01_populacao);
+    //Cálculos
+    B01_DensidadePopulacional = B01_populacao/B01_area;
+    B01_PIB_perCapita = B01_PIB/B01_populacao;
+    B02_DensidadePopulacional = B02_populacao/B02_area;
+    B02_PIB_perCapita = B02_PIB/B02_populacao;
+    B03_DensidadePopulacional = B03_populacao/B03_area;
+    B03_PIB_perCapita = B03_PIB/B03_populacao;
+    B04_DensidadePopulacional = B04_populacao/B04_area;
+    B04_PIB_perCapita = B04_PIB/B04_populacao;
 
-    printf ("Carta 2: %s\n", &B02_nome);
-    printf ("População: %.2f\n", &B02_populacao);
-    printf ("PIB: %.2f\n", &B02_PIB);
-    printf ("Área: %.2f\n", &B02_area);
-    printf ("Pontos turísticos: %d\n", &B02_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &B02_DensidadePopulacional, B02_DensidadePopulacional = B02_populacao/B02_area);
-    printf ("PIB per Capita: %.2f\n", &B02_PIB_perCapita, B02_PIB_perCapita = B02_PIB/B02_populacao);
+    printf ("***Confirmação dos dados:***\n");
+    printf ("Carta 1: %s\n", B01_nome);
+    printf ("População: %.2f\n", B01_populacao);
+    printf ("PIB: %.2f\n", B01_PIB);
+    printf ("Área: %.2f\n", B01_area);
+    printf ("Pontos turísticos: %d\n", B01_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", B01_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", B01_PIB_perCapita);
+
+    printf ("Carta 2: %s\n", B02_nome);
+    printf ("População: %.2f\n", B02_populacao);
+    printf ("PIB: %.2f\n", B02_PIB);
+    printf ("Área: %.2f\n", B02_area);
+    printf ("Pontos turísticos: %d\n", B02_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", B02_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", B02_PIB_perCapita);
     
-    printf ("Carta 3: %s\n", &B03_nome);
-    printf ("População: %.2f\n", &B03_populacao);
-    printf ("PIB: %.2f\n", &B03_PIB);
-    printf ("Área: %.2f\n", &B03_area);
-    printf ("Pontos turísticos: %d\n", &B03_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &B03_DensidadePopulacional, B03_DensidadePopulacional = B03_populacao/B03_area);
-    printf ("PIB per Capita: %.2f\n", &B03_PIB_perCapita, B03_PIB_perCapita = B03_PIB/B03_populacao);
+    printf ("Carta 3: %s\n", B03_nome);
+    printf ("População: %.2f\n", B03_populacao);
+    printf ("PIB: %.2f\n", B03_PIB);
+    printf ("Área: %.2f\n", B03_area);
+    printf ("Pontos turísticos: %d\n", B03_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", B03_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", B03_PIB_perCapita);
     
-    printf ("Carta 4: %s\n", &B04_nome);
-    printf ("População: %.2f\n", &B04_populacao);
-    printf ("PIB: %.2f\n", &B04_PIB);
-    printf ("Área: %.2f\n", &B04_area);
-    printf ("Pontos turísticos: %d\n", &B04_PontosTuristicos);
-    printf ("Densidade Populacional: %.2f\n", &B04_DensidadePopulacional, B04_DensidadePopulacional = B04_populacao/B04_area);
-    printf ("PIB per Capita: %.2f\n", &B04_PIB_perCapita, B04_PIB_perCapita = B04_PIB/B04_populacao);
+    printf ("Carta 4: %s\n", B04_nome);
+    printf ("População: %.2f\n", B04_populacao);
+    printf ("PIB: %.2f\n", B04_PIB);
+    printf ("Área: %.2f\n", B04_area);
+    printf ("Pontos turísticos: %d\n", B04_PontosTuristicos);
+    printf ("Densidade Populacional: %.2f\n", B04_DensidadePopulacional);
+    printf ("PIB per Capita: %.2f\n", B04_PIB_perCapita);
 
   // Área para exibição dos dados da cidade
 
@@ -231,6 +250,7 @@ int main() {
 
 
     return 0;
+
 
 
 
